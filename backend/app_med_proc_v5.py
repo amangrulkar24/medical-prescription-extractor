5e033c63722ac6e7edbe35b5382d777c9efca3c7
Atharva <atharva.mangrulkar24@iimb.ac.in>
2025-07-02 15:42:35 +0530

    Update app_med_proc_v5.py

--- backend/app_med_proc_v5.py
+++ backend/app_med_proc_v5.py
@@ -17,11 +17,12 @@ import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
 # === Setup ===
+app = Flask(__name__)
+CORS(app, origins="*", supports_credentials=True)
+
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-app = Flask(__name__)
-CORS(app, origins=["https://rx-sage.vercel.app"])
 
 app.config["DEBUG"] = True
 
